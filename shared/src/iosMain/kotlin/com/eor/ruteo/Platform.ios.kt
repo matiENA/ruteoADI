a3fcf3943ff3047340ec7d24001afc4a89cdd5d9
Matias <analistaena@gmail.com>
2026-06-05 15:07:53 -0300

    Fix: Agregar implementacion de cache (Settings) para iOS

--- shared/src/iosMain/kotlin/com/eor/ruteo/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/eor/ruteo/Platform.ios.kt
@@ -1,9 +1,21 @@
 package com.eor.ruteo
 
 import platform.UIKit.UIDevice
+import com.russhwolf.settings.NSUserDefaultsSettings
+import com.russhwolf.settings.Settings
+import platform.Foundation.NSUserDefaults
 
+// 1. Esto probablemente ya lo tenías (para detectar la versión de iOS)
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+// ==========================================
+// 2. 👇 ESTO ES LO QUE FALTABA PARA LA CACHÉ EN iOS 👇
+// ==========================================
+actual fun createSettingsFactory(): Settings {
+    // Usamos NSUserDefaults, que es el estándar nativo de iOS para guardar configuraciones/caché
+    return NSUserDefaultsSettings(NSUserDefaults.standardUserDefaults)
+}
